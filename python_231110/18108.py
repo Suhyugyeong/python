e7c9c50cd869a66feb1155f843e2d84233e2dd02
Suhyugyeong <suhyugyeongsuh@gamil.com>
2023-11-10 15:25:33 +0900

    python

---
+++ python_231110/18108.py
@@ -0,0 +1,7 @@
+#태국 연도(불기) 입력을 받는다.
+#숫자 연산을 위해 str을 int 형 변환
+bulgi = int(input())
+#연산진행
+seogi = bulgi - 543
+#서기 출력
+print(seogi)
---
+++ python_231110/condition.py
@@ -0,0 +1,19 @@
+# msg = input()
+
+# is_number =False
+# if input("숫자를 입력했나요? 맞으면1, 아니면 2 \n") == "1":
+#     msg = int(msg)
+#     if msg:
+#         print(msg)
+#     else:
+#         print("false인 경우에는 출력되지 않습니다.")
+        
+print(bool(1))
+print(bool("안녕"))
+print(bool("파이선"))
+print(bool(-5))
+print(bool(0.025))
+print(bool(" "))
+print(bool(""))
+print(bool("누가 쉽대?"))
+print(bool(78))
